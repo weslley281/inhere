363271dea69620d0197f0d81ff0b5527b8ad791b
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-08-01 12:30:38 -0300

    useState

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,68 +1,81 @@
-import { Text, TextInput, View, TouchableOpacity, FlatList, Alert } from 'react-native';
+import { useState } from 'react';
+import {
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Alert,
+} from 'react-native';
 import { Participant } from '../../components/Participant';
 import { styles } from './styles';
 
 export function Home() {
-    const participants = ['a'];
+  const [participants, setParticipants] = useState(['Weslley']);
 
-    function handlePaticipantAdd() {
-        if(participants.includes("Weslley1")){
-            return Alert.alert("Erro ao Cadastrar", "Já exite um participante com esse nome");
-        }
+  function handlePaticipantAdd() {
+    if (participants.includes('a')) {
+      return Alert.alert(
+        'Erro ao Cadastrar',
+        'Já exite um participante com esse nome'
+      );
     }
 
-    function handlePaticipantRemove(name: string){
-        Alert.alert("Alerta", `Você tem certeza que quer remover o participante ${name} ?`, [
-            {
-                text: "OK",
-                onPress: () => Alert.alert(`Você deletou ${name}`)
-            },
-            {
-                text: "Cancelar"
-            }
-        ]);
-    }
+    setParticipants(prevState => [...prevState , 'Gotosao']);
+  }
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.eventName}>Novo Evento</Text>
-
-            <Text style={styles.eventDate}>
-                By Weslley Ferraz
-            </Text>
-
-            <View style={styles.form}>
-                <TextInput
-                style={styles.imput}
-                placeholder="Nome do participante"
-                placeholderTextColor="#6B6B6B"
-                />
-
-                <TouchableOpacity style={styles.button} onPress={handlePaticipantAdd}>
-                    <Text style={styles.buttonText}>
-                        +
-                    </Text>
-                </TouchableOpacity>
-            </View>
-
-            <FlatList
-                style={styles.campoRolagem}
-                keyExtractor={item => item}
-                data={participants}
-                renderItem={({ item }) => (
-                    <Participant
-                        key={item}
-                        name={item}
-                        onRemove={() => handlePaticipantRemove(item)}
-                    />
-                )}
-                showsVerticalScrollIndicator={false}
-                ListEmptyComponent={() => (
-                    <Text style={styles.listEmpytText}>
-                        Ninguém quer ir nesse evento, seu trouxa
-                    </Text>
-                )}
-            />
-        </View>
+  function handlePaticipantRemove(name: string) {
+    Alert.alert(
+      'Alerta',
+      `Você tem certeza que quer remover o participante ${name} ?`,
+      [
+        {
+          text: 'OK',
+          onPress: () => Alert.alert(`Você deletou ${name}`),
+        },
+        {
+          text: 'Cancelar',
+        },
+      ]
     );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Novo Evento</Text>
+
+      <Text style={styles.eventDate}>By Weslley Ferraz</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.imput}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handlePaticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        style={styles.campoRolagem}
+        keyExtractor={(item) => item}
+        data={participants}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handlePaticipantRemove(item)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmpytText}>
+            Ninguém quer ir nesse evento, seu trouxa
+          </Text>
+        )}
+      />
+    </View>
+  );
 }
